2e0302a353452a1d4ea92b508c8a77f14d35a64a
sardorkobilzonov <sardorkobilzonov4@gmail.com>
2024-11-09 17:11:46 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,113 @@
+//1-masala
+// {
+//   let a = 15,
+//     b = 4;
+//   while (a >= b) {
+//     a = a - b;
+//   }
+//   console.log(a);
+// }
+
+//2-masala
+// {
+//   let a = 15,
+//     b = 4,
+//     sum = 0;
+//   while (a >= b) {
+//     a = a - b;
+//     sum++;
+//   }
+//   console.log(sum);
+// }
+
+//3-masala
+// {
+//   let n = 17,
+//     k = 5,
+//     b = 0;
+//   while (n >= k) {
+//     n -= k;
+//     b++;
+//   }
+//   console.log("Butun qismi ", b);
+
+//   console.log("Qoldiq ", n);
+// }
+
+//4-masala
+// {
+//   let n = 9,
+//     a = 3;
+//   while (n >= 2) {
+//     n = n - a;
+//   }
+//   console.log(`${a} ning darajasi`);
+// }
+
+//5-masala
+
+// {
+//   let n = 4,
+//     k = 0;
+//   while (n >= 2) {
+//     n /= 2;
+//     k++;
+//   }
+//   console.log(k);
+// }
+
+//6-masala
+
+// {
+//   let n = 8,
+//     a = 1;
+//   while (n > 1) {
+//     a *= n;
+//     n -= 2;
+//   }
+//   console.log(a);
+// }
+
+//7-masala
+
+// {
+//   let n = 20,
+//     k = 0;
+//   while (k * k <= n) {
+//     k++;
+//   }
+//   console.log(k);
+// }
+
+//8-masala
+
+// {
+//   let n = 20,
+//     k = 1;
+//   while ((k + 1) * (k + 1) <= n) {
+//     k++;
+//   }
+//   console.log(k);
+// }
+
+//9-masala
+
+// {
+//   let n = 15,
+//     k = 1;
+//   while (3 * k <= n) {
+//     k++;
+//   }
+//   console.log(k);
+// }
+
+//10-masala
+
+// {
+//   let n = 28,
+//     k = 0;
+//   while (3 * (k + 1) <= n) {
+//     k++;
+//   }
+//   console.log(k);
+// }
